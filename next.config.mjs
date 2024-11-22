5b13bda5ade981cd15b13fcc831126c3534f0a4d
Fabricio Pagliarini <fabriciopgl87@gmail.com>
2024-11-21 21:42:10 -0300

    🚀 Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 
 const nextConfig = {
   output: 'export',
-  trailingSlash: true
+  trailingSlash: true,
+  basePath: '/turismo-serra-gaucha', 
+  assetPrefix: '/turismo-serra-gaucha',
 }
 
 export default nextConfig;
